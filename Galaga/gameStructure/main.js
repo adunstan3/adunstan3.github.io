777178f140e23c723f4b17727a966f782888b141
Andrew Dunstan <andrewdunstan@ip0af525e8.int.uvm.edu>
2022-01-26 14:02:29 -0500

    putting each project in it's own folder

--- Galaga/gameStructure/main.js
+++ Galaga/gameStructure/main.js
@@ -14,27 +14,27 @@ var myDatabase;
 var scoreThreshold = 10;
 
 function preload() {
-  playerImage = loadImage('assets/playerShip.png');
-  skullImage = loadImage('assets/skull.png');
+  playerImage = loadImage('Galaga/assets/playerShip.png');
+  skullImage = loadImage('Galaga/assets/skull.png');
 
-  explosionImages.push(loadImage('assets/Explosion1.png'));
-  explosionImages.push(loadImage('assets/Explosion2.png'));
-  explosionImages.push(loadImage('assets/Explosion3.png'));
-  explosionImages.push(loadImage('assets/Explosion4.png'));
+  explosionImages.push(loadImage('Galaga/assets/Explosion1.png'));
+  explosionImages.push(loadImage('Galaga/assets/Explosion2.png'));
+  explosionImages.push(loadImage('Galaga/assets/Explosion3.png'));
+  explosionImages.push(loadImage('Galaga/assets/Explosion4.png'));
 
-  enemyExplosionImages.push(loadImage('assets/enemyExplosion1.png'));
-  enemyExplosionImages.push(loadImage('assets/enemyExplosion2.png'));
-  enemyExplosionImages.push(loadImage('assets/enemyExplosion3.png'));
-  enemyExplosionImages.push(loadImage('assets/enemyExplosion4.png'));
-  enemyExplosionImages.push(loadImage('assets/enemyExplosion5.png'));
+  enemyExplosionImages.push(loadImage('Galaga/assets/enemyExplosion1.png'));
+  enemyExplosionImages.push(loadImage('Galaga/assets/enemyExplosion2.png'));
+  enemyExplosionImages.push(loadImage('Galaga/assets/enemyExplosion3.png'));
+  enemyExplosionImages.push(loadImage('Galaga/assets/enemyExplosion4.png'));
+  enemyExplosionImages.push(loadImage('Galaga/assets/enemyExplosion5.png'));
 
-  enemyImages["bossHealthy"] = [loadImage('assets/EnemyBossHealthy1.png'),
-                                  loadImage('assets/EnemyBossHealthy2.png')];
-  enemyImages["bossHurt"] = [loadImage('assets/EnemyBossHurt1.png'),
-                                  loadImage('assets/EnemyBossHurt2.png')];
-  enemyImages["blue"] = [loadImage('assets/EnemyBlue1.png'),
+  enemyImages["bossHealthy"] = [loadImage('Galaga/assets/EnemyBossHealthy1.png'),
+                                  loadImage('Galaga/assets/EnemyBossHealthy2.png')];
+  enemyImages["bossHurt"] = [loadImage('Galaga/assets/EnemyBossHurt1.png'),
+                                  loadImage('Galaga/assets/EnemyBossHurt2.png')];
+  enemyImages["blue"] = [loadImage('Galaga/assets/EnemyBlue1.png'),
                                   loadImage('assets/EnemyBlue2.png')];
-  enemyImages["red"] = [loadImage('assets/EnemyRed1.png'),
+  enemyImages["red"] = [loadImage('Galaga/assets/EnemyRed1.png'),
                                   loadImage('assets/EnemyRed2.png')];
 }
 
